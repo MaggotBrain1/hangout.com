713edbbba196f3e5d0cdcef6f20d26d7631506eb
MaggotBrain1 <75130836+MaggotBrain1@users.noreply.github.com>
2022-07-25 22:03:00 +0200

    [Hangout] fixe change status

--- src/Controller/HangoutController.php
+++ src/Controller/HangoutController.php
@@ -11,6 +11,7 @@ use App\Form\HangoutFormType;
 use App\Repository\HangoutRepository;
 use App\Repository\PlaceRepository;
 use App\Repository\StatusRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -58,7 +59,8 @@ class HangoutController extends AbstractController
             $hangouts = $hangoutRepository->findByFilter($campus,$name,$startDate,$endDate,$imOrginizer,$imIn,$imNotIn,$pastHangout,$user);
         }else{
             $hangouts = $hangoutRepository->findHangoutAvaible();
-            $this->updateStatusOfHangouts($hangouts,$statusRepository,$em);
+/*            $this->updateStatusOfHangouts($hangouts,$statusRepository,$em);*/
+            $this->updateStatusOfHangoutsV2($hangouts,$statusRepository,$em);
         }
 
         return $this->render('hangout/hangoutList.html.twig', [
@@ -339,5 +341,51 @@ class HangoutController extends AbstractController
             'form' => $hangoutForm->createView(),
         ]);
     }*/
+
+    public function updateStatusOfHangoutsV2($allHangouts, StatusRepository $statusRepository, EntityManagerInterface $em){
+
+        foreach ($allHangouts as $hg){
+            $hangoutClone = clone($hg);
+            $startDate = $hg->getStartTime();
+            $now = (new \DateTime("now")); // renvoi la date/h/m/s du jour
+            $durationH = $hg->getDuration()->format('H');// on récupere les minutes de la durée
+            $durationM = $hg->getDuration()->format('i');//on récupere les heures de la durée
+            $endDate = clone($startDate);// on clone $startDate pour que les modification ne l'affecte pas directement.
+            $endDate->modify("+{$durationM} minutes");// on ajoute les minutes à la date de début
+            $endDate->modify("+{$durationH} hour");// on ajoute les heures à la date de départ
+            // $endDate === la date et heures de la sortie additionnée au temps de durée de la sorite
+            $oneMothMore = clone($startDate);
+            $oneMothMore->modify('+1 month');
+
+          switch($hg->getStatus()->getId()){
+                case ($startDate <= $now && $now < $endDate) :
+                    if ($hg->getStatus()->getId() != Status::STATUS_IN_PROGRESS){
+                        $hg->setStatus($statusRepository->find(Status::STATUS_IN_PROGRESS));
+                        echo "sortie en cours";
+                    }
+                    break;
+                case ($now > $endDate && $now < $oneMothMore ) :
+                    if($hg->getStatus()->getId() != Status::STATUS_PAST){
+                        $hg->setStatus($statusRepository->find(Status::STATUS_PAST));
+                        echo "sortie en passée";
+                    }
+                    break;
+                case ($now > $oneMothMore):
+                    if ($hg->getStatus()->getId() != Status::STATUS_ARCHIVED) {
+                        $hg->setStatus($statusRepository->find(Status::STATUS_ARCHIVED));
+                        echo "sortie en Archivée";
+                        dump("coucou on passe dans le jour est plus grans que la date + 1 moi");
+
+                    }
+                    break;
+          }
+
+            if ($hangoutClone->getStatus()->getId() != $hg->getStatus()->getId()){
+                $em->persist($hg);
+                $em->flush();
+            }
+
+        }
+    }
 
 }
